22fda4f4b7d85d9fad2a9a7ab23cb78d3ef4b188
Varun Prohit <20163@iiitu.ac.in>
2024-01-08 14:31:09 +0530

    initial file

---
+++ gen.py
@@ -0,0 +1,21 @@
+import textwrap
+from dotenv import dotenv_values
+from IPython.display import Markdown, display
+import google.generativeai as genai
+import platform
+class GenerativeAI:
+    CONST_COMMAND = "Show command for the given query. Show only command not any other description with it. Also show text as 'invalid input' if user provide wrong information. Also the os is "
+    CONST_OS = platform.system()
+    def __init__(self):
+        env_vars = dotenv_values(".env")
+        self.GOOGLE_API_KEY = env_vars.get('GOOGLE_API_KEY')
+        self.model = genai.GenerativeModel('gemini-pro')
+        genai.configure(api_key=self.GOOGLE_API_KEY)
+
+    def to_markdown(self, text):
+        text = text.replace('•', '  *')
+        return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    
+    def generate_response(self, user_input):
+        response = self.model.generate_content(self.CONST_COMMAND + self.CONST_OS + user_input)
+        return response.text
---
+++ genTrial.py
@@ -0,0 +1,36 @@
+import pathlib
+import textwrap
+import os
+import google.generativeai as genai
+
+# Used to securely store your API key
+# from google.colab import userdata
+from dotenv import dotenv_values
+from IPython.display import display
+from IPython.display import Markdown
+
+env_vars = dotenv_values(".env")
+# GOOGLE_API=os.getenv('GOPATH')
+# to do : add google_api_key  in os env var 
+GOOGLE_API_KEY=env_vars.get('GOOGLE_API_KEY') 
+
+# print(GOOGLE_API)
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+
+model = genai.GenerativeModel('gemini-pro')
+
+commandPrompt = "Show command for the given query. Show only command not any other description with it. Also show text as 'invalid input' if user provide wrong information."
+userInput = "open file named vp"
+
+response = model.generate_content(commandPrompt + userInput)
+
+output = to_markdown(response.text)
+display(response.text)
+print(response.text)
+
